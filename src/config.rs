0f898e6da3e5b3aa730be47e85fa8b0f59f5161f
WXH <wenxihua@users.noreply.github.com>
2025-07-27 17:12:28 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["home.hotting.fun:21000"];
+pub const RS_PUB_KEY: &str = "VExjcb2+RQZGPrpihL4AecHh3Ap3oOKCbsKb9XoSPY8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
